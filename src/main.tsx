aa2c06c99fae3ed849167500a413be6ebabc81b8
aniketjdesign <aniketjart@gmail.com>
2024-11-08 00:36:14 +0530

    zipy integration

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
+import zipy from 'zipyai'; 
+zipy.init('7129b133');
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -11,3 +13,4 @@ createRoot(document.getElementById('root')!).render(
     </BrowserRouter>
   </StrictMode>
 );
+
