2957dbbf64166b5236877ce9c837a77eb435dc35
aniketjdesign <aniketjart@gmail.com>
2024-11-09 01:51:24 +0530

    removed keep alive and start workout button change

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,8 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import zipy from 'zipyai'; 
-import { setupKeepAlive } from './utils/keepAlive';
 
 zipy.init('7129b133');
-
-// Setup keep-alive functionality
-setupKeepAlive();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
